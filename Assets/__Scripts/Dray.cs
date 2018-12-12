4e62696922010eba257e8e7e5ab20b5272c3859e
mm2461 <43255719+mm2461@users.noreply.github.com>
2018-12-12 10:10:39 -0500

    walk and attack

--- Assets/__Scripts/Dray.cs
+++ Assets/__Scripts/Dray.cs
@@ -4,10 +4,17 @@ using UnityEngine;
 public class Dray : MonoBehaviour
 
 {
+    public enum eMode { idle, move, attack, transition } // a
     [Header("Set in Inspector")]
     public float speed = 5;
+    public float attackDuration = 0.25f;// Number of seconds to attack
+    public float attackDelay = 0.5f; // Delay between attacks
     [Header("Set Dynamically")]
     public int dirHeld = -1; // Direction of the held movement key
+    public int facing = 1; // Direction Dray is facing
+    public eMode mode = eMode.idle; // a
+    private float timeAtkDone = 0; // b
+    private float timeAtkNext = 0; // c
     private Rigidbody rigid;
     private Animator anim;
     private Vector3[] directions = new Vector3[] {
@@ -21,15 +28,57 @@ KeyCode.UpArrow, KeyCode.LeftArrow, KeyCode.DownArrow }; // a
     }
     void Update()
     {
+        //————Handle Keyboard Input and manage eDrayModes————
         dirHeld = -1;
-        // Delete the four "if ( Input.GetKey..." lines that were here
         for (int i = 0; i < 4; i++)
         {
-            if (Input.GetKey(keys[i])) dirHeld = i; // b
+            if (Input.GetKey(keys[i])) dirHeld = i;
         }
+        // Pressing the attack button(s)
+        if (Input.GetKeyDown(KeyCode.Z) && Time.time >= timeAtkNext)
+        { // a
+            mode = eMode.attack;
+            timeAtkDone = Time.time + attackDuration;
+            timeAtkNext = Time.time + attackDelay;
+        }
+        //Finishing the attack when it's over
+        if (Time.time >= timeAtkDone)
+        { // b
+            mode = eMode.idle;
+        }
+        //Choosing the proper mode if we're not attacking
+        if (mode != eMode.attack)
+        { // c
+            if (dirHeld == -1)
+            {
+                mode = eMode.idle;
+            }
+            else
+            {
+                facing = dirHeld; // d
+                mode = eMode.move;
+            }
+        }
+        //————Act on the current mode————
         Vector3 vel = Vector3.zero;
-        if (dirHeld > -1) vel = directions[dirHeld];
+        switch (mode)
+        { // e
+            case eMode.attack:
+                anim.CrossFade("Dray_Attack_" + facing, 0);
+                anim.speed = 0;
+                break;
+            case eMode.idle:
+                anim.CrossFade("Dray_Walk_" + facing, 0);
+                anim.speed = 0;
+                break;
+            case eMode.move:
+                vel = directions[dirHeld];
+                anim.CrossFade("Dray_Walk_" + facing, 0);
+                anim.speed = 1;
+                break;
+        }
         rigid.velocity = vel * speed;
+    
         // Animation
         if (dirHeld == -1)
         { // b
