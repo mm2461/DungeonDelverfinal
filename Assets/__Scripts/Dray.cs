10a48ca14a4c672c41358c85b430f49614a32e60
mm2461 <43255719+mm2461@users.noreply.github.com>
2018-12-12 09:53:41 -0500

    walking

--- Assets/__Scripts/Dray.cs
+++ Assets/__Scripts/Dray.cs
@@ -9,6 +9,7 @@ public class Dray : MonoBehaviour
     [Header("Set Dynamically")]
     public int dirHeld = -1; // Direction of the held movement key
     private Rigidbody rigid;
+    private Animator anim;
     private Vector3[] directions = new Vector3[] {
 Vector3.right, Vector3.up, Vector3.left, Vector3.down };
     private KeyCode[] keys = new KeyCode[] { KeyCode.RightArrow,
@@ -16,6 +17,7 @@ KeyCode.UpArrow, KeyCode.LeftArrow, KeyCode.DownArrow }; // a
     void Awake()
     {
         rigid = GetComponent<Rigidbody>();
+        anim = GetComponent<Animator>();
     }
     void Update()
     {
@@ -28,5 +30,16 @@ KeyCode.UpArrow, KeyCode.LeftArrow, KeyCode.DownArrow }; // a
         Vector3 vel = Vector3.zero;
         if (dirHeld > -1) vel = directions[dirHeld];
         rigid.velocity = vel * speed;
+        // Animation
+        if (dirHeld == -1)
+        { // b
+            anim.speed = 0;
+        }
+        else
+        {
+            anim.CrossFade("Dray_Walk_" + dirHeld, 0); // c
+            anim.speed = 1;
+        }
     }
 }
+    
